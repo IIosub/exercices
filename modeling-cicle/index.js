41253b3543ab1473961a0cba153a22f0c6240b23
IIosub <114613610+IIosub@users.noreply.github.com>
2023-08-27 19:50:21 +0100

    circle area and circumference

---
+++ modeling-cicle/index.js
@@ -0,0 +1,18 @@
+//Complete the following program to add the circle object definition. Its radius value is input by the user.
+
+const r = Number(prompt("Enter the circle radius:"));
+
+// TODO: create the circle object here
+//Math.PI * r ** 2, area  2 * Math.PI * r circ
+
+const circle = {
+  circumference() {
+    return 2 * Math.PI * r;
+  },
+  area() {
+    return Math.PI * r ** 2;
+  },
+};
+
+console.log(`Its circumference is ${circle.circumference()}`);
+console.log(`Its area is ${circle.area()}`);
