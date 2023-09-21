36587f967d2885d134e26c28aca21ef6b1117a59
IIosub <114613610+IIosub@users.noreply.github.com>
2023-09-21 14:13:18 +0100

    exercice solved

---
+++ newspaper-List/index.js
@@ -0,0 +1,24 @@
+function showNews() {
+  const newsPapers = [
+    "https://www.nytimes.com",
+    "https://www.washingtonpost.com",
+    "http://www.economist.com",
+  ];
+
+  const containerElement = document.getElementById("content");
+  console.log(containerElement);
+
+  newsPapers.forEach((newspaperUrl) => {
+    const linkElement = document.createElement("a");
+
+    linkElement.setAttribute("href", newspaperUrl);
+
+    linkElement.style.display = "block";
+
+    linkElement.textContent = newspaperUrl;
+
+    containerElement.appendChild(linkElement);
+  });
+}
+
+showNews();
